91a602a2f55f44974bc53476d2a6806244b11404
robi-y <robi@post.jce.ac.il>
2015-05-06 11:14:54 +0300

    git-upadte

--- git-update.bat
+++ git-update.bat
@@ -3,7 +3,7 @@ SET MESSAGE=%1
 IF [%1]==[] SET MESSAGE= "update site"
 git status
 git add -A
-git commit -m %MESSAGE%
+git commit -m "%MESSAGE%"
 git checkout gh-pages
 git merge master
 git checkout master
