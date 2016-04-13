64362bf864851954b9457583f9c9fc4ebe59c971
itamar sharify <itamarsharify@gmail.com>
2016-04-13 15:21:05 +0300

    added mongo to server

--- tirgul/full-server-client-example/public/js/app.js
+++ tirgul/full-server-client-example/public/js/app.js
@@ -1,4 +1,5 @@
 (function() {
+    
     var app = angular.module('store', ['ngRoute', 'ngAnimate']);
 
     app.controller('MainController', function($scope){
@@ -7,14 +8,70 @@
     .controller('galleryController', function($scope) {
         this.images = images;
     })
-    .controller('formController', function($scope) {
-        this.submitForm = function(){
-            users.push(this.user);
-            this.user = {};
-            console.log(users);
+    .service('RestService', function($http){
+   var getAllUsers = function(){
+       var request = {
+           method:'GET',
+           url:'/users'
+       };
+       return $http(request);
+   };
+   var registerUser = function(userJson){
+     var request = {
+         method:'POST',
+         url:'/reg',
+         data:userJson
+     };
+       return $http(request);
+   };
+        var deleteUser = function(userJson){
+            var request = {
+                method:'POST',
+                url:'/delete',
+                data:userJson
+            };
+            return $http(request);
+        }
+    
+    return {
+        getAllUsers:getAllUsers,
+        registerUser:registerUser,
+        deleteUser:deleteUser
+    };
+})
+    .controller('formController', function($scope,RestService) {
+        $scope.user = {};
+        $scope.users = {};
+        $scope.getAllUsers = function(){
+             RestService.getAllUsers()
+            .then(function(response){
+                $scope.users = response.data;
+                $scope.$apply;
+            })
+        }
+        $scope.submitForm = function(){
+        $scope.error = "";    
+        RestService.registerUser($scope.user)
+            .then(function successCallback(response) {
+            console.log('OK');
+            $scope.user = {};
+            $scope.getAllUsers();
+        });
         };
+        $scope.delete = function(){
+            $scope.error = "";
+            RestService.deleteUser($scope.user)
+            .then(function(data){
+                console.log(data.data);
+                $scope.getAllUsers();
+                $scope.user = {};
+            })
+            .catch(function(data){
+               $scope.error = data.data; 
+            });
+        }
     });
-    var users = [{}];
+   
     var images = [
         '/images/1.jpg',
         '/images/2.jpg',
@@ -31,8 +88,7 @@
         })
             .when('/login', {
             templateUrl: 'login.html',
-            controller: 'formController',
-            controllerAs: 'form'
+            controller: 'formController'
         })
         .when('/home', {
             templateUrl:'home.html',
