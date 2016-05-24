b34e41b44e8e14f7470d60cd99e35e56ed62dba4
itamar sharify <itamarsharify@gmail.com>
2016-05-24 09:19:45 +0300

    added cookies pres

--- tirgul/azure-based-server/New folder/SIT/app.js
+++ tirgul/azure-based-server/New folder/SIT/app.js
@@ -1,37 +1,13 @@
-/**
- * [require system needed stuff
- */
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var mongoose = require('mongoose'); //get DB
-var config = require('config.json')('config/config.json'); //get config json
-var Schema = mongoose.Schema; //to create Schema
-var db = config.db;
-var mongoAddr = db.mongodb;
-var mongoAddress = 'mongodb://' + db.user + ':' + db.password + mongoAddr.host + ':' + mongoAddr.port + '/' + db.appName;
-var connection = mongoose.createConnection(mongoAddress); //connect to the db server
-//Schema = document
-var users = new Schema({
-  name: String,
-  sureName: String
-});
-var User = connection.model('User', users);
-module.exports = User;
-/**
- * System Routes
- * routes - API calls
- * users = the user API
- * home - not used for now
- */
+var mongoose = require('mongoose');
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var home = require('./routes/home');
 
-//create express app
 var app = express();
 
 // view engine setup
@@ -41,27 +17,16 @@ app.set('view engine', 'jade');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json()); //get POST data
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: false
 }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', routes);
+app.use('/users', users);
 
-/*
- * routes
- * when in '/' use both routes, and users
- * when in '/home' use home
- */
-app.use('/route', routes);
-app.use('/', users);
-app.use('/home', home);
-
-
-/**
- * JADE auto error handlers
- */
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
--- tirgul/azure-based-server/public/routes/users.js
+++ tirgul/azure-based-server/public/routes/users.js
@@ -1,22 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var User = require('User');
 /*
-*DATA BASE
-*/
-var mongoose = require('mongoose'); //get DB
-var Schema = mongoose.Schema; //to create Schema
-var connection = mongoose.createConnection('mongodb://testdb:testdb@ds032319.mlab.com:32319/jce16');//connect to the db server
-//Schema = document
-var users = new Schema({
-  name:  String,
-  sureName: String
-});
+ *DATA BASE
+ */
 
 //model = collection
-var User = connection.model('User', users);
+
+
 /*
-* INNER API CALLS
-*/
+ * INNER API CALLS
+ */
 //register new user
 router.post('/reg', function(req, res) {
     var js = req.body; //get the json sent from user
@@ -29,11 +23,11 @@ router.post('/reg', function(req, res) {
 
     var user = new User(userJson); //create new db instance
     //save the new user to db
-    user.save(function(err){
-        if(err)
+    user.save(function(err) {
+        if (err)
             res.status(500).end("error"); //response on error with status code 500
         else
-            res.status(200).end("Added", userJson, "to db");//response on success  with status code 200
+            res.status(200).end("Added", userJson, "to db"); //response on success  with status code 200
 
     });
 });
@@ -48,27 +42,27 @@ router.post('/delete', function(req, res) {
     }
 
     //save the new user to db
-    User.remove(userJson, function(err){
-        if(err)
+    User.remove(userJson, function(err) {
+        if (err)
             res.status(500).end("error"); //response on error with status code 500
         else
-            res.status(200).end("removed", userJson, "from db");//response on success  with status code 200
+            res.status(200).end("removed", userJson, "from db"); //response on success  with status code 200
 
     });
 });
 //get all users
-router.get('/users', function(req, res){
+router.get('/users', function(req, res) {
     //find all users in db
-     User.find({}, function(err, users) {
-        var userMap = {};//return object
+    User.find({}, function(err, users) {
+        var userMap = {}; //return object
 
         //fill up the object
         users.forEach(function(user) {
-          userMap[user._id] = user;
+            userMap[user._id] = user;
         });
-//    return the users object
-    res.end(JSON.stringify(userMap, null, "\t"));
-  });
+        //    return the users object
+        res.end(JSON.stringify(userMap, null, "\t"));
+    });
 });
 
 module.exports = router;
